af46e397db5c46d8a3b477db247de947dc72db0f
Pablo Grant <grantpablo4@gmail.com>
2024-03-09 19:46:47 -0500

    add runner config

---
+++ runner.sh
@@ -0,0 +1,3 @@
+mkdir actions-runner && cd actions-runner
+curl -o actions-runner-osx-x64-2.314.1.tar.gz -L https://github.com/actions/runner/releases/download/v2.314.1/actions-runner-osx-x64-2.314.1.tar.gz
+tar xzf ./actions-runner-osx-x64-2.314.1.tar.gz
